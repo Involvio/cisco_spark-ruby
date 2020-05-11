3dfdd7ed19b281a2db2455b46b819ea104f0ddb8
Srikanth Kurni <srikanth@involvio.com>
2020-05-12 00:08:56 +0530

    Update attributes for person resource

--- lib/cisco_spark/models/person.rb
+++ lib/cisco_spark/models/person.rb
@@ -8,9 +8,17 @@ module CiscoSpark
     attributes(
       id: DataCaster::String,
       emails: DataCaster::Array,
+      phone_numbers: DataCaster::Array,
       display_name: DataCaster::String,
+      nick_name: DataCaster::String,
+      first_name: DataCaster::String,
+      last_name: DataCaster::String,
       avatar: DataCaster::String,
+      org_id: DataCaster::String,
       created: DataCaster::DateTime,
+      last_activity: DataCaster::DateTime,
+      status: DataCaster::String,
+      type: DataCaster::String,
     )
 
     def self.all_by_email(email, options={})
